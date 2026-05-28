bdd5adb41b72d4c5fea3f3283380b835a9660cfb
Rono <rowanpsmith@yahoo.com.au>
2026-05-29 01:04:04 +1000

    feat: enhance MimicStrategy with randomized fuse and mimic behaviors

--- src/main/java/dev/rono/igniscore/strategies/MimicStrategy.java
+++ src/main/java/dev/rono/igniscore/strategies/MimicStrategy.java
@@ -3,7 +3,6 @@ package dev.rono.igniscore.strategies;
 import dev.rono.igniscore.model.BlockDefinition;
 import dev.rono.igniscore.model.RuntimeBlockInstance;
 import org.bukkit.Location;
-import org.bukkit.Sound;
 import org.bukkit.entity.TNTPrimed;
 import org.bukkit.util.Vector;
 
@@ -13,30 +12,41 @@ public class MimicStrategy extends BaseBlockBehaviorStrategy {
         BlockDefinition def = instance.getDefinition();
         Location loc = instance.getLocation().toCenterLocation();
         
-        int mimicCount = getCustomInt(def, "mimicCount", 4);
-        double horizontalPower = getCustomDouble(def, "mimicHorizontalPower", 0.5);
-        double verticalPower = getCustomDouble(def, "mimicVerticalPower", 0.2);
+        // Remove display entity immediately so the block disappears on ignition
+        if (instance.getDisplayEntity() != null) {
+            instance.getDisplayEntity().remove();
+            instance.setDisplayEntity(null);
+        }
 
-        for (int i = 0; i < mimicCount; i++) {
+        int mimicCount = getCustomInt(def, "mimicCount", 8);
+        double horizontalPower = getCustomDouble(def, "mimicHorizontalPower", 1.0);
+        double verticalPower = getCustomDouble(def, "mimicVerticalPower", 0.5);
+
+        int totalCount = mimicCount + 1;
+        int realIndex = (int) (Math.random() * totalCount);
+
+        for (int i = 0; i < totalCount; i++) {
             TNTPrimed tnt = loc.getWorld().spawn(loc, TNTPrimed.class);
-            tnt.setFuseTicks(instance.getTicksLeft());
+            
+            // Randomize fuse slightly for more unpredictability
+            int fuse = def.getFuse() + (int) (Math.random() * 40 - 20);
+            tnt.setFuseTicks(Math.max(10, fuse));
+            
             tnt.setVelocity(new Vector(
                     (Math.random() - 0.5) * horizontalPower,
-                    verticalPower,
+                    verticalPower + (Math.random() * 0.4),
                     (Math.random() - 0.5) * horizontalPower
             ));
-            tnt.setYield(0);
-        }
-        // Randomize fuse for unpredictability
-        int fuse = def.getFuse();
-        instance.setTicksLeft(fuse + (int) (Math.random() * 60 - 30));
-    }
 
-    @Override
-    public void onTrigger(RuntimeBlockInstance instance, Object context) {
-        BlockDefinition def = instance.getDefinition();
-        Location loc = instance.getLocation().toCenterLocation();
-        float power = (float) getCustomDouble(def, "power", 4.0);
-        loc.getWorld().createExplosion(loc, power, getCustomBoolean(def, "fire", false), getCustomBoolean(def, "blockDamage", true));
+            if (i == realIndex) {
+                tnt.setYield((float) getCustomDouble(def, "power", 4.0));
+                tnt.setIsIncendiary(getCustomBoolean(def, "fire", false));
+            } else {
+                tnt.setYield(0);
+            }
+        }
+
+        // Trigger immediate removal of the runtime instance as the behavior is now handled by entities
+        instance.setTicksLeft(0);
     }
 }
